6a77f86db85e534e2b7873f10ab09f76597eab4d
Samy Tamim <samytamim42@gmail.com>
2025-04-06 15:57:50 +0100

    feat(parser): remove pointers in config file schema

--- internal/parser/config/config.go
+++ internal/parser/config/config.go
@@ -8,10 +8,10 @@ import (
 )
 
 type Program struct {
-	Command   *string `toml:"command" validate:"required"`
-	Autostart *bool   `toml:"autostart" validate:"default=true"`
+	Command   string `toml:"command" validate:"required"`
+	Autostart bool   `toml:"autostart" validate:"default=true"`
 	// Autorestart   string `toml:"autorestart"`
-	NumProcs *int `toml:"numprocs" validate:"default=1,min=1"`
+	NumProcs int `toml:"numprocs" validate:"default=1,min=1"`
 	// StdoutLogFile string `toml:"stdout_logfile`
 	// StderrLogFile string `toml:"stderr_logfile"`
 }
@@ -23,21 +23,21 @@ type Config struct {
 func Parse_config(file string) (Config, error) {
 	var conf Config
 
-	_, err := toml.DecodeFile(file, &conf)
+	mdata, err := toml.DecodeFile(file, &conf)
 
-	err_msg := validate(reflect.ValueOf(&conf))
+	err_msg := validate(reflect.ValueOf(&conf), []string{}, mdata)
 	if err_msg != nil {
 		return conf, err_msg
 	}
 
 	for name, program := range conf.Programs {
 		fmt.Printf("Program: %s\n", name)
-		fmt.Printf("\tCommand: %s\n", *program.Command)
-		// fmt.Printf("\tAutostart: %s\n", *program.Autostart)
-		// fmt.Printf("\tAutorestart: %s\n", *program.Autorestart)
-		// fmt.Printf("\tNumProcs: %s\n", *program.NumProcs)
-		// fmt.Printf("\tStdoutLogFile: %s\n", *program.StdoutLogFile)
-		// fmt.Printf("\tStderrLogFile: %s\n", *program.StderrLogFile)
+		fmt.Printf("\tCommand: %s\n", program.Command)
+		fmt.Printf("\tAutostart: %s\n", program.Autostart)
+		// fmt.Printf("\tAutorestart: %s\n", program.Autorestart)
+		fmt.Printf("\tNumProcs: %s\n", program.NumProcs)
+		// fmt.Printf("\tStdoutLogFile: %s\n", program.StdoutLogFile)
+		// fmt.Printf("\tStderrLogFile: %s\n", program.StderrLogFile)
 	}
 
 	return conf, err
--- internal/parser/config/validator.go
+++ internal/parser/config/validator.go
@@ -6,6 +6,8 @@ import (
 	"slices"
 	"strconv"
 	"strings"
+
+	"github.com/BurntSushi/toml"
 )
 
 const (
@@ -23,7 +25,7 @@ func handleMin(field string, tag_val string, value reflect.Value) (bool, string)
 	if err != nil {
 		return false, "Malformed schema. min must be a number"
 	}
-	return num <= value.Elem().Interface().(int), fmt.Sprintf("%s must be less than %v", field, tag_val)
+	return num <= value.Interface().(int), fmt.Sprintf("%s must be less than %v", field, tag_val)
 }
 
 func handleMax(field string, tag_val string, value reflect.Value) (bool, string) {
@@ -31,26 +33,21 @@ func handleMax(field string, tag_val string, value reflect.Value) (bool, string)
 	if err != nil {
 		return false, "Malformed schema. max must be a number"
 	}
-	return num >= value.Elem().Interface().(int), fmt.Sprintf("%s must be less than %v", field, tag_val)
+	return num >= value.Interface().(int), fmt.Sprintf("%s must be less than %v", field, tag_val)
 }
 
 func handleEnum(field string, tag_val string, value reflect.Value) (bool, string) {
 	vals := strings.Split(tag_val, "|")
-	return slices.Contains(vals, value.Elem().Interface().(string)), fmt.Sprintf("%s must be in %v", field, vals)
-}
-
-func handleRequired(field string, tag_val string, value reflect.Value) (bool, string) {
-	return !value.IsNil(), fmt.Sprintf("%s is required", field)
+	return slices.Contains(vals, value.Interface().(string)), fmt.Sprintf("%s must be in %v", field, vals)
 }
 
 var schemaFunctions = map[string]func(string, string, reflect.Value) (bool, string){
-	REQUIRED: handleRequired,
-	MIN:      handleMin,
-	MAX:      handleMax,
-	ENUM:     handleEnum,
+	MIN:  handleMin,
+	MAX:  handleMax,
+	ENUM: handleEnum,
 }
 
-func validate(v reflect.Value) error {
+func validate(v reflect.Value, path []string, md toml.MetaData) error {
 	if v.Kind() == reflect.Ptr || v.Kind() == reflect.Interface {
 		v = v.Elem()
 	}
@@ -59,23 +56,27 @@ func validate(v reflect.Value) error {
 		for _, key := range v.MapKeys() {
 			value := v.MapIndex(key)
 			if value.Kind() == reflect.Struct || value.Kind() == reflect.Ptr {
-				err := validate_struct(value.Elem())
+				path = append(path, key.String())
+				err := validate_struct(value.Elem(), path, md)
+				path = path[:len(path)-1]
 				if err != nil {
 					return err
 				}
 			}
 		}
 	case reflect.Struct:
-		return validate_struct(v)
+		return validate_struct(v, path, md)
 	}
 	return nil
 }
 
-func validate_struct(v reflect.Value) error {
+func validate_struct(v reflect.Value, path []string, md toml.MetaData) error {
 	for i := 0; i < v.NumField(); i++ {
 		field_name := v.Type().Field(i).Name
 		field_value := v.Field(i)
 		tag := v.Type().Field(i).Tag.Get("validate")
+		toml_tag := v.Type().Field(i).Tag.Get("toml")
+		path := append(path, toml_tag)
 		if tag != "" {
 			rules := strings.Split(tag, ",")
 			for _, rule := range rules {
@@ -85,41 +86,49 @@ func validate_struct(v reflect.Value) error {
 				if len(rule_data) > 1 {
 					rule_value = rule_data[1]
 				}
-				if rule_name != DEFAULT {
-					cond, err := schemaFunctions[rule_name](field_name, rule_value, field_value)
-					if !cond {
-						return fmt.Errorf(err)
+				con := md.IsDefined(path...)
+				switch rule_name {
+				case REQUIRED:
+					if !con {
+						return fmt.Errorf("%s is required", field_name)
 					}
-				} else if field_value.IsNil() {
-					new_value := reflect.New(field_value.Type().Elem())
-					if new_value.Elem().Kind() == reflect.Bool {
+				case DEFAULT:
+					if con {
+						break
+					}
+					switch field_value.Kind() {
+					case reflect.Bool:
 						bool_v, err := strconv.ParseBool(rule_value)
 						if err != nil {
 							return fmt.Errorf("Malformed schema. Default doesnt follow the field type: boolean\n")
 						}
-						new_value.Elem().Set(reflect.ValueOf(bool_v))
-					} else if new_value.Elem().Kind() == reflect.Int {
+						field_value.Set(reflect.ValueOf(bool_v))
+					case reflect.Int:
 						num, err := strconv.Atoi(rule_value)
 						if err != nil {
 							return fmt.Errorf("Malformed schema. Default doesnt follow the field type: boolean\n")
 						}
-						new_value.Elem().Set(reflect.ValueOf(num))
-					} else if new_value.Elem().Kind() == reflect.String {
-						new_value.Elem().Set(reflect.ValueOf(rule_value))
-					} else {
-						return fmt.Errorf("Malformed schema. Unknown type. cant match default value with given field type %v\n", new_value.Kind())
+						field_value.Set(reflect.ValueOf(num))
+					case reflect.String:
+						field_value.Set(reflect.ValueOf(rule_value))
+					default:
+						return fmt.Errorf("Malformed schema. Unknown type. cant match default value with given field type %v\n", field_value.Kind())
 					}
-					field_value.Set(new_value)
-					break
+				default:
+					cond, err := schemaFunctions[rule_name](field_name, rule_value, field_value)
+					if !cond {
+						return fmt.Errorf(err)
+					}
 				}
 			}
 		}
 		if field_value.Kind() == reflect.Map || field_value.Kind() == reflect.Struct {
-			err := validate(field_value)
+			err := validate(field_value, path, md)
 			if err != nil {
 				return err
 			}
 		}
+		path = path[:len(path)-1]
 	}
 	return nil
 }
