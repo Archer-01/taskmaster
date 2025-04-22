3bab827d3e069c89e16135bf45b4aaac811e850c
Hamza Haddani <hhamza@student.1337.ma>
2025-04-22 14:59:52 +0100

    Revert "feat(server): Add autostart support in config file"
    This reverts commit d1ff473b3c97fc77cc227578f49fde777d42caf1.

--- internal/manager/manager.go
+++ internal/manager/manager.go
@@ -10,7 +10,6 @@ import (
 
 type JobManager struct {
 	Jobs []*job.Job
-	Config *config.Config
 }
 
 func Init(conf config.Config) *JobManager {
@@ -22,34 +21,17 @@ func Init(conf config.Config) *JobManager {
 	}
 
 	manager.Jobs = jobs
-
-	manager.Config = &conf
-
 	return &manager
 }
 
 func (ins *JobManager) Start() {
 	for _, j := range ins.Jobs {
-		autostart := ins.Config.Programs[j.Name].Autostart
-
-		if autostart == false {
-			continue
-		}
-
 		j.StartJob()
 	}
 }
 
 func (ins *JobManager) Finish() {
 	for _, j := range ins.Jobs {
-		// HACK: This is a temporary fix
-		// - Jobs should have a state (Started, Stopped, Running, ...etc)
-		// - Status must be checked before trying to finish a job
-		autostart := ins.Config.Programs[j.Name].Autostart
-		if !autostart {
-			continue
-		}
-
 		fmt.Fprintf(os.Stdout, "Exiting [%s]\n", j.Name)
 		err := j.Command.Process.Kill()
 		if err != nil {
